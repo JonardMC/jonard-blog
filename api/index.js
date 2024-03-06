0cf5684cf182ae4b2c9148712874a340faef15d6
Jonard Carganilla <jonard.m.carganilla@lpstech.com>
2024-03-06 17:37:04 +0800

    firebase from back

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
 
+
 dotenv.config();
 
 mongoose
@@ -19,6 +20,7 @@ mongoose
 
 const app = express();
 
+
 app.use( express.json());
 
 app.listen(3000, () => {
@@ -26,14 +28,14 @@ app.listen(3000, () => {
 });
 
 app.use('/api/user', userRoutes );
-app.use('api/auth', authRoutes);
+app.use('/api/auth', authRoutes);
 
-app.use((err,req,res,next)=>{
-    const statusCode = err.statusCode || 500;
-    const message = err.message || 'Internal Server Error';
-    res.status(statusCode).json ({
-        success: false,
-        statusCode,
-        message
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
     }) ;
 });
